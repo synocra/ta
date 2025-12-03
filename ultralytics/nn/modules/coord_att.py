e0efef3dccc19fc99fffaefdf49ebd8c5a09e2de
synocra <mukhlisnurhidayat09@gmail.com>
2025-12-03 08:59:00 +0700

    add yaml for YOLO11+CA

--- ultralytics/nn/modules/coord_att.py
+++ ultralytics/nn/modules/coord_att.py
@@ -37,8 +37,6 @@ class CoordAtt(nn.Module):
         self.pool_w = nn.AdaptiveAvgPool2d((1, None))
 
         mip = max(8, inp // reduction)
-
-        # ✅ Pastikan input conv1 = inp (BUKAN angka tetap seperti 512)
         self.conv1 = nn.Conv2d(inp, mip, kernel_size=1, stride=1, padding=0)
         self.bn1 = nn.BatchNorm2d(mip)
         self.act = h_swish()
@@ -50,19 +48,16 @@ class CoordAtt(nn.Module):
         identity = x
         n, c, h, w = x.size()
 
-        # pooling arah horizontal dan vertikal
         x_h = self.pool_h(x)
         x_w = self.pool_w(x).permute(0, 1, 3, 2)
 
-        # gabungkan dan proses conv1
+
         y = torch.cat([x_h, x_w], dim=2)
         y = self.act(self.bn1(self.conv1(y)))  # ✅ conv1 input dynamic
 
-        # pisahkan kembali
         x_h, x_w = torch.split(y, [h, w], dim=2)
         x_w = x_w.permute(0, 1, 3, 2)
 
-        # apply coordinate attention
         a_h = torch.sigmoid(self.conv_h(x_h))
         a_w = torch.sigmoid(self.conv_w(x_w))
         out = identity * a_h * a_w
